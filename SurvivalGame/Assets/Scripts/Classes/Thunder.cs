722f840526c5c651bab26627d736ade2c06b7647
Anh Hoang <anhh34711@gmail.com>
2023-03-02 22:29:08 +0700

    upcode

---
+++ SurvivalGame/Assets/Scripts/Classes/Thunder.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : MonoBehaviour
+{
+    public int Damanaged {get; set;}
+    public int CoolDown {get; set;}
+    public int Level {get; set;}
+
+    public Thunder()
+    {
+        Damanaged = 25 * 3;
+        CoolDown = 3;
+        Level = 1;
+    }
+}
