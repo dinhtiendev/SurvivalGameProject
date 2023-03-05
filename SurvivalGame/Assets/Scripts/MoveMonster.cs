8ea5093e61a95a08ff49ae27f8aa46b36c7560a9
Hoangvippro <hoangnmhe153543@fpt.edu.vn>
2023-03-05 12:58:09 +0700

    fix_monstermove

--- SurvivalGame/Assets/Scripts/MoveMonster.cs
+++ SurvivalGame/Assets/Scripts/MoveMonster.cs
@@ -13,14 +13,14 @@ public class MoveMonster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position- transform.position;
+        Vector2 direction = player.transform.position - transform.position;
 
         direction.Normalize();
 
