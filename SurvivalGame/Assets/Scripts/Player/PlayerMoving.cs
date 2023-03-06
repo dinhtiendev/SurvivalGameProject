6ffb23c09020f763740a0651c971fd1342ee248b
Dinh Tien <82441050+dinhtiendev@users.noreply.github.com>
2023-03-06 13:36:07 +0700

    Update Player moving and Add Camera follow Player

--- SurvivalGame/Assets/Scripts/Player/PlayerMoving.cs
+++ SurvivalGame/Assets/Scripts/Player/PlayerMoving.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class PlayerMoving : MonoBehaviour
 {
-    public GameObject player;
-    public FixedJoystick Joystick;
+    FixedJoystick Joystick;
     Rigidbody2D rb;
     Vector2 move;
     public float moveSpeed;
-    GameObject playerObject;
     // Start is called before the first frame update
     void Start()
     {
-        
-        playerObject = Instantiate(player);
-        playerObject.transform.position = new Vector3(0, 0, 0);
-        playerObject.transform.rotation = Quaternion.identity;
-        rb = playerObject.GetComponent<Rigidbody2D>();
+        Joystick = (FixedJoystick)FindObjectOfType(typeof(FixedJoystick));
+        transform.position = new Vector3(0, 0, 0);
+        transform.rotation = Quaternion.identity;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -29,7 +26,7 @@ public class PlayerMoving : MonoBehaviour
         float hAxis = move.x;
         float vAxis = move.y;
         float zAxis = Mathf.Atan2(hAxis, vAxis) * Mathf.Rad2Deg;
-        playerObject.transform.eulerAngles = new Vector3(0f, 0f, -zAxis);
+        transform.eulerAngles = new Vector3(0f, 0f, -zAxis);
         rb.MovePosition(rb.position + move * moveSpeed * Time.deltaTime);
     }
 }
