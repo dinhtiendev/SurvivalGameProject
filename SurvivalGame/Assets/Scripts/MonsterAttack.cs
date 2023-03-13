53acc4c87ca01920173cb943657d6205d05ef593
Dinh Tien <82441050+dinhtiendev@users.noreply.github.com>
2023-03-13 19:21:07 +0700

    Update Monster Attack

--- SurvivalGame/Assets/Scripts/MonsterAttack.cs
+++ SurvivalGame/Assets/Scripts/MonsterAttack.cs
@@ -6,22 +6,14 @@ public class MonsterAttack : MonoBehaviour
 {
     public int damage = 10;  // Lượng máu mà người chơi mất khi va chạm với quái vật
 
-	void OnCollisionEnter(Collision collision)
-	{
+	
 
-		if (collision.gameObject.tag == "Player")
-		{
-			Player playerController = collision.gameObject.GetComponent<Player>();
-			playerController.TakeDamage(damage);
-		}
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject.tag == "Player")
-		{
-			Player playerController = other.gameObject.GetComponent<Player>();
-			playerController.TakeDamage(damage);
-		}
-	}
+	//void OnTriggerEnter2D(Collision2D other)
+	//{
+	//	if (other.gameObject.tag == "Player")
+	//	{
+	//		Player playerController = other.gameObject.GetComponent<Player>();
+	//		playerController.TakeDamage(damage);
+	//	}
+	//}
 }
