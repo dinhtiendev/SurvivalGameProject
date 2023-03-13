a53a33679a506c32a2e017319882bffa1c1f2a76
Hoangvippro <hoangnmhe153543@fpt.edu.vn>
2023-03-13 14:55:30 +0700

    monster_attacked

---
+++ SurvivalGame/Assets/Scripts/MonsterAttack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAttack : MonoBehaviour
+{
+    public int damage = 10;  // Lượng máu mà người chơi mất khi va chạm với quái vật
+
+	void OnCollisionEnter(Collision collision)
+	{
+
+		if (collision.gameObject.tag == "Player")
+		{
+			Player playerController = collision.gameObject.GetComponent<Player>();
+			playerController.TakeDamage(damage);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Player playerController = other.gameObject.GetComponent<Player>();
+			playerController.TakeDamage(damage);
+		}
+	}
+}
