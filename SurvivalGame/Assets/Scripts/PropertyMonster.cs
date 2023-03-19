a043350cb58073a56acfe3f25e5c2a9a2e96a32b
Anh Hoang <anhh34711@gmail.com>
2023-03-20 00:33:37 +0700

    save_load

---
+++ SurvivalGame/Assets/Scripts/PropertyMonster.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyMonster 
+{
+    public TypeMonster typeMonster { set; get; }
+    public int Health { get; set; }
+    public int Damanaged { get; set; }
+    public int Speed { get; set; }
+    public int Exp { get; set; }
+    
+}
+public enum TypeMonster
+    {
+    MonsterX, MonsterFlash, MonsterTanker
+    }
